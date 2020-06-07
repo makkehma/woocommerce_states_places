2ceef32e19ccc8d3d21fff618ee9bc778a3c6606
makkehma <66426147+makkehma@users.noreply.github.com>
2020-06-07 09:17:37 -0400

    Lebanon States

---
+++ templates/states/LB.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Lebanon
+ * - 6 Gouvernotes
+ * 
+ * Source: 
+ * - [Liban Post Back End Data and Wikiperdai for Codings]
+ *
+ * @author  Mohamad Ali MAKKEH <makkehma@hotmail.fr> | https://gmartgroup.com/
+ * @version 1.0.0
+ * @license http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+global $states;
+
+$states['LB'] = array(
+  'BA' => 'Beirut',
+  'AS' => 'North Lebanon,
+  'JL' => 'Mont Liban',
+  'JA' => 'South Lebanon',
+  'NA' => 'Nabatieh',
+  'BI' => 'Bekaa'
+);
+return $states;
